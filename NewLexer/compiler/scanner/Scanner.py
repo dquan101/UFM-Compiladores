b8c00bbb2201880c618bdca3777203917c20f092
Diego Quan <diegoquan101@gmail.com>
2019-09-24 15:01:06 -0600

    Fixed 0, shlex "", and operations

--- NewLexer/compiler/scanner/Scanner.py
+++ NewLexer/compiler/scanner/Scanner.py
@@ -25,8 +25,9 @@ CFA = {0:{'(':1, ')':1, '[':1, ']':1, '{':1, '}':1},
 EFA = {0:{';':1},
 	   1:{}}
 
-OFA = {0:{'<':1, '=':1, '>':1, '+':1, '-':1, '/':1, '*':1, '%':1, '^':1},
-	   1:{}}
+OFA = {0:{'<':2, '=':1, '>':2, '+':2, '-':2, '/':1, '*':1, '%':1, '^':1,},
+	   1:{},
+	   2:{'=':1, '':1}}
 
 SCFA = {0:{39:1},
 	   1:{39:2, 32:1, 33:1, 35:1, 36:1, 37:1, 38:1, 40:1, 41:1, 42:1, 43:1, 44:1, 45:1, 46:1, 47:1, 48:1, 49:1, 50:1, 51:1, 52:1, 53:1, 54:1, 55:1, 56:1, 57:1, 58:1, 59:1, 60:1, 61:1, 62:1, 63:1, 64:1, 65:1, 66:1, 67:1, 68:1, 69:1, 70:1, 71:1, 72:1, 73:1, 74:1, 75:1, 76:1, 77:1, 78:1, 79:1, 80:1, 81:1, 82:1, 83:1, 84:1, 85:1, 86:1, 87:1, 88:1, 89:1, 90:1, 91:1, 93:1, 94:1, 95:1, 96:1, 97:1, 98:1, 99:1, 100:1, 101:1, 102:1, 103:1, 104:1, 105:1, 106:1, 107:1, 108:1, 109:1, 110:1, 111:1, 112:1, 113:1, 114:1, 115:1, 116:1, 117:1, 118:1, 119:1, 120:1, 121:1, 121:1, 123:1, 124:1, 125:1, 126:1},
@@ -47,14 +48,14 @@ def type(listt, types):
 			matrix.append(lists)
 		else:
 			try:
-				if accepts(HFA, 0, {2, 3, 4, 7, 8, 9}, x) in (2, 7):
+				if accepts(HFA, 0, {2, 3, 4, 7, 8, 9, 0}, x) in (2, 7):
 					if listt[y-1] == '-':
 						x = '-' + x
 					lists[0] = 'hexadecimal'
 					lists[1] = x
 					matrix.append(lists)
 					aceptado = True
-				if accepts(HFA, 0, {2, 3, 4, 7, 8, 9}, x) in (3, 4, 8, 9):
+				if accepts(HFA, 0, {2, 3, 4, 7, 8, 9, 0}, x) in (3, 4, 8, 9):
 					if listt[y-1] == '-':
 						x = '-' + x
 					if listt[y+1] == '.':
@@ -157,9 +158,12 @@ def raw(text):
 def tokenize(file):
 	with open(file, 'r') as f:
 		newlist = []
-		for line in f:
-			str = list(shlex.shlex(line))
-			newlist.extend(str)
+		try:
+			for line in f:
+				str = list(shlex.shlex(line))
+				newlist.extend(str)
+		except ValueError:
+			print('Syntax Error. No closing quotation')
 	return newlist
 
 def accepts(transitions,initial,accepting,s):
