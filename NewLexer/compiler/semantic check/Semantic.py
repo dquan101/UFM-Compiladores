96c55920be04e05a19fce8e57565a84cad3f7d68
Diego Quan <diegoquan101@gmail.com>
2019-10-10 11:20:36 -0600

    added location

--- NewLexer/compiler/semantic check/Semantic.py
+++ NewLexer/compiler/semantic check/Semantic.py
@@ -1,8 +1,14 @@
 from ast import literal_eval
 
 outF = open("parser/token.txt", "r")
+outL = open("scanner/location.txt", "r")
+locations = outL.readlines()
 contents = outF.readlines()
+location = []
 new_contents = []
+for i in locations:
+    line = literal_eval(i)
+    location.append(line)
 for i in contents:
     line = literal_eval(i)
     new_contents.append(line)
@@ -13,4 +19,10 @@ for i in new_contents:
         scope += 1
     if i[0] == 'ID' and i[1] != 'Program' and i[1] != 'main':
         i.append(scope)
-        print(i)
+
+for x in new_contents:
+    for i in location:
+        if x[0] == 'ID':    
+            if x[1] in i[0]:
+                x.append(i[1])
+                print(x)
